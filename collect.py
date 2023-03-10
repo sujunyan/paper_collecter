9403ec613491eb4556bcadb3f739e82eda466b86
Junyan Su <junyan.su@my.cityu.edu.hk>
2023-03-10 00:52:05 +0000

    bug fixes

--- collect.py
+++ collect.py
@@ -8,6 +8,7 @@ import config
 import datetime
 from abstract import add_abstract
 import abstract
+import psutil
 
 def save_one_item(dd):
     doi = dd["DOI"]
@@ -17,7 +18,11 @@ def save_one_item(dd):
         return
 
     if not "abstract" in dd:
-        add_abstract(dd)
+        try:
+            add_abstract(dd)
+        except: 
+            print(f"Failed to add abstract for {file_path}.")
+            return
 
     js_obj = json.dumps(dd, indent=4)
     with open(file_path, "w") as f:
@@ -41,5 +46,26 @@ for jour in journal_list:
         save_one_item(x)
         print(x["title"][0])
 
-abstract.driver.quit()
+
+# A robust way to quit Firefox driver.
+# Refer to https://stackoverflow.com/questions/48703734/why-does-the-python-selenium-webdriver-quit-not-quit
+driver = abstract.driver
+driver_process = psutil.Process(driver.service.process.pid)
+
+if driver_process.is_running():
+    print ("driver is running")
+
+    firefox_process = driver_process.children()
+    if firefox_process:
+        firefox_process = firefox_process[0]
+
+        if firefox_process.is_running():
+            print("Firefox is still running, we can quit")
+            driver.quit()
+        else:
+            print("Firefox is dead, can't quit. Let's kill the driver")
+            firefox_process.kill()
+    else:
+        print("driver has died")
+
 print("done.")
